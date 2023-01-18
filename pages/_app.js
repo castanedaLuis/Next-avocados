d6fdfb14cb8937071546de278e4b0a09aaa0a775
castanedaLuis <castanedaLuis@github.com>
2023-01-18 13:42:00 -0600

    reportWebVitals  para medir las metricas de performance que nos ofrece next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import Layout from '@components/Layout/index';
 
 
-//import CartProvider from '@store/Cart'
+export function reportWebVitals(metric) {
+  console.log(metric)
+  // analytics - calibre
+  // serverAnulicts.log(metric)
+}
 
 const MyApp = ({ Component, pageProps }) => {
   // Aditional props
