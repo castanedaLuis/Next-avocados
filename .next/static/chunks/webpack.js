a45415b48287ccb07e256a80e6a32451b17a8591
castanedaLuis <castanedaLuis@github.com>
2023-01-18 23:30:56 -0600

    creando redux

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0fa1404ee2134e3d"; }
+/******/ 		__webpack_require__.h = function() { return "e4eca0ea68beba77"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ actions/types.js
@@ -0,0 +1,3 @@
+export const SET_AVOCADOS = 'SET_AVOCADOS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_FAVORITE = 'SET_FAVORITE';
